5b063c6afe2ab6f88a7f63dbf19043ffb0f60807
miki0033 <75072281+miki0033@users.noreply.github.com>
2024-05-14 10:56:29 +0200

    fix logo ing

--- src/components/molecules/Logo.tsx
+++ src/components/molecules/Logo.tsx
@@ -3,11 +3,7 @@ import Avatar from "@mui/material/Avatar";
 const Logo = () => {
   return (
     <div className="flex justify-center rounded-full overflow-hidden w-full">
-      <Avatar
-        alt="logo"
-        src="../../../public/logo-only.png"
-        sx={{ width: 56, height: 56 }}
-      />
+      <Avatar alt="logo" src="./logo-only.png" sx={{ width: 56, height: 56 }} />
     </div>
   );
 };
